e497eaae4212934a4466ca4f8997dee64eb160c3
Lucas <57920717+L64@users.noreply.github.com>
2020-07-12 14:27:55 -0300

    Update footer_white.php

--- includes/footer_white.php
+++ includes/footer_white.php
@@ -4,5 +4,5 @@
     });
 </script>
 <div class="footer_white" style="margin:0px 15%;padding: 15px; color: #fff; text-align: center;border-top: 1px solid #1f648e;">
-    Wallstant &copy; All rights reserved.
+    Sargga is a fork of <a href="https://wallstant.github.io">Wallstant</a>, thx to the creator 4 a big part of the code
 </div>
