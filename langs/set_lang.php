a9af63ef40fbdfde9ada527ab70d3188cd007da2
Lucas <57920717+L64@users.noreply.github.com>
2020-07-12 12:24:37 -0300

    Update set_lang.php

--- langs/set_lang.php
+++ langs/set_lang.php
@@ -6,6 +6,9 @@ case 'English':
 case 'العربية':
 	include_once $path."langs/arabic.php";
 	break;
+case 'Español':
+	include_once $path."langs/spanish.php";
+	break;
 default:
 	$_SESSION['language'] = "English";
 	include_once $path."langs/english.php";
