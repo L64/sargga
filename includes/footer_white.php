c6027327a933ba0e6621f6d65f4a307df0a849d9
Lucas <57920717+L64@users.noreply.github.com>
2020-09-13 12:08:23 -0300

    Update footer_white.php

--- includes/footer_white.php
+++ includes/footer_white.php
@@ -6,5 +6,5 @@
 <div class="footer_white" style="margin:0px 15%;padding: 15px; color: #fff; text-align: center;border-top: 1px solid #1f648e;">
     <p>Wallstant &copy; All rights reserved.</p>
     <p>This Wallstant copy was modificated to use by Team Ciber</p>
-    <p><a href="/blog">Blog</a> - <a href="https://invite.gg/ciber">Discord</a> - <a href="https://taringa.net/+ciber">Taringa (+ciber)</a> - <a href="https://mas.to/@ciber">Mastodon (@ciber@mas.to)</a></p>
+    <p><a href="https://blog.ciber.repl.co">Blog</a> - <a href="https://invite.gg/ciber">Discord</a> - <a href="https://taringa.net/+ciber">Taringa (+ciber)</a> - <a href="https://mas.to/@ciber">Mastodon (@ciber@mas.to)</a></p>
 </div>
