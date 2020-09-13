224b8fa2208be875264c2c0daa28591a355341fc
Lucas <57920717+L64@users.noreply.github.com>
2020-09-13 12:03:59 -0300

    Update footer_white.php

--- includes/footer_white.php
+++ includes/footer_white.php
@@ -4,5 +4,7 @@
     });
 </script>
 <div class="footer_white" style="margin:0px 15%;padding: 15px; color: #fff; text-align: center;border-top: 1px solid #1f648e;">
-    Sargga is a fork of <a href="https://wallstant.github.io">Wallstant</a>, thx to the creator 4 a big part of the code
+    <p>Wallstant &copy; All rights reserved.</p>
+    <p>This Wallstant copy was modificated to use by Team Ciber</p>
+    <p><a href="/blog">Blog</a> - <a href="https://invite.gg/ciber">Discord</a> - <a href="https://taringa.net/+ciber">Taringa (+ciber)</a> - <a href="https://mas.to/@ciber">Mastodon (@ciber@mas.to)</a></p>
 </div>
