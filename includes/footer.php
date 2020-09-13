c6f88848fb01995ab0ca2d9a97b90d3efc5e8e4d
Lucas <57920717+L64@users.noreply.github.com>
2020-09-13 12:06:03 -0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -12,5 +12,5 @@ border-top: 1px solid rgb(213, 213, 213);
 ">
     <p>Wallstant &copy; All rights reserved.</p>
     <p>This Wallstant copy was modificated to use by Team Ciber</p>
-    <p><a href="/blog">Blog</a> - <a href="https://invite.gg/ciber">Discord</a> - <a href="https://taringa.net/+ciber">Taringa (+ciber)</a> - <a href="https://mas.to/@ciber">Mastodon (@ciber@mas.to)</a></p>
+    <p><a href="https://blog.ciber.repl.co">Blog</a> - <a href="https://invite.gg/ciber">Discord</a> - <a href="https://taringa.net/+ciber">Taringa (+ciber)</a> - <a href="https://mas.to/@ciber">Mastodon (@ciber@mas.to)</a></p>
 </div>
