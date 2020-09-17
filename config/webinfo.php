79bef397b78d328a28f6386a822c1b3b30ceda64
Lucas <57920717+L64@users.noreply.github.com>
2020-09-17 09:30:21 -0300

    Add files via upload

---
+++ config/webinfo.php
@@ -0,0 +1,12 @@
+<?php
+    # NO USAR TODAVIA!!!!
+    
+    # Name of the social network
+    $webpage_name = "ʟ𝗰𝗶𝗯𝗿";
+    # Description
+    $webpage_description = "Lcibr is an irani-latinoamerican social network platform oriented to technology, but open to all, for now the registers are only for the people of our Discord.";
+    # Search keywords
+    $keywords = "social network,social media,Wallstant,meet,free platform,Lcibr,Sargga";
+    # Username prefix
+    $prefix = "/";
+?>
